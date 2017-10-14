1a2ff7d4ae723675e08a9e1d06b27772fb1c3e7e
myth52125 <myth52125@hotmail.com>
2017-10-14 20:42:58 +0800

    change

--- tree/binaryTree.h
+++ tree/binaryTree.h
@@ -1,19 +1,22 @@
 #ifndef _MYTH52125_BINARYTREE_H_
 #define _MYTH52125_BINARYTREE_H_
 
+#include "Node.h"
+
 namespace myth52125
 {
 namespace algorithm
 {
 
-class 
+class BinaryTree
 {
 public:
-
+    BinaryTree();
+    ~BinaryTree();
 private:
-
+    Node *_root;
 public:
-
+    void add(Node &node);
 };
 
 
